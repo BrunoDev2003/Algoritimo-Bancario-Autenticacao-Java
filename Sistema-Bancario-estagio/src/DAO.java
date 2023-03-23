3a71754ddf762ab10bd2085ab50dbc71f11369a9
Bruno Gusmão <brunogusmao43@gmail.com>
2023-03-23 16:55:32 -0300

    .

--- Sistema-Bancario-estagio/src/DAO.java
+++ Sistema-Bancario-estagio/src/DAO.java
@@ -1,4 +1,11 @@
 
 public class DAO {
-
+	public Usuario pesquisaUsuarioLogin(String login, String senha) {
+		Usuario usuario = new Usuario();
+		usuario.setLogin(login);
+		usuario.setSenha(senha);
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		return null;
+		
+	}
 }
--- Sistema-Bancario-estagio/src/Usuario.java
+++ Sistema-Bancario-estagio/src/Usuario.java
@@ -1,4 +1,14 @@
 
 public class Usuario {
 
+	public void setLogin(String login) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setSenha(String senha) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
