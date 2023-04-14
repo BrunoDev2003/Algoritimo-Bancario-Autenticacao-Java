36edf9f33debb2b6ab926512fe1ebd3dcf8f9cbe
Bruno Gusmão <brunogusmao43@gmail.com>
2023-04-14 18:18:13 -0300

    implementing the session factory

--- Sistema-Bancario-estagio/src/SessionFactory.java
+++ Sistema-Bancario-estagio/src/SessionFactory.java
@@ -1,4 +1,18 @@
+import javax.naming.ConfigurationException;
 
 public class SessionFactory {
+	static SessionFactory sessionFactory = null;
 
+    static {
+        try{
+            sessionFactory = new ConfigurationException().configure().buildSessionFactory();
+        }catch (Throwable ex) { 
+           System.err.println("Failed to create sessionFactory object." + ex);
+           throw new ExceptionInInitializerError(ex); 
+        }
+    }
+
+    public static void addToDatabase(String something) {
+        
+    }
 }
